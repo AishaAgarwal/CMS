703a25cd93800f7232e11b8c3b1022cb3bba2964
AishaAgarwal <aishaagarwal19567@gmail.com>
2024-06-09 11:27:24 +0530

    feat: modal to display contact information

--- client/src/pages/AllContact.js
+++ client/src/pages/AllContact.js
@@ -1,43 +1,78 @@
 import { useEffect, useState } from "react";
 import Spinner from "../components/Spinner";
+import { Modal } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 
 const AllContact = () => {
-    const [loading, setLoading] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [modalData, setModalData] = useState({});
   const [contacts, setContacts] = useState([]);
+ 
 
   useEffect(() => {
     setLoading(true);
-    const fetchContacts = async() => {
-    try {
-      const res = await fetch(`http://localhost:8000/api/mycontacts`, {
-        method: "GET",
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem("token")}`,
-        },
-      });
-      const result = await res.json();
-      if (!result.error) {
-        console.log(result);
-        setContacts(result.contacts);
+    const fetchContacts = async () => {
+      try {
+        const res = await fetch(`http://localhost:8000/api/mycontacts`, {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        });
+        const result = await res.json();
+        if (!result.error) {
+          
+          setContacts(result.contacts);
+          
 
-         setLoading(false);
-      } else {
-        console.log(result);
-         setLoading(false);
+          setLoading(false);
+        } else {
+          console.log(result);
+          setLoading(false);
+        }
+      } catch (err) {
+        console.log(err);
       }
-    } catch (err) {
-      console.log(err);
-    }
-};
-fetchContacts();
+    };
+    fetchContacts();
   }, []);
+
+  const deleteContact = async (id) => {
+    if (window.confirm("are you sure you want to delete this contact ?")){
+      try{
+        const res = await fetch(`https://localhost:8000/api/delete/${id}`, {
+          method: "DELETE",
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+
+          },
+        });
+
+        const result = await res.json();
+        if (!result.error) {
+          setContacts(result.myContacts);
+         
+          setShowModal(false);
+        }
+        else{
+          console.log(result.error);
+        }
+      }
+      catch(err){
+        console.log(err);
+      }
+    }
+  };
   return (
     <>
       <div>
         <h1> Your Contacts</h1>
-        {loading ? <Spinner splash="Loading Contacts..."/>:(
-            <table className="table table-hover">
+        {loading ? (
+          <Spinner splash="Loading Contacts..." />
+        ) : (
+          <table className="table table-hover">
             <thead>
               <tr className="table-light">
                 <th scope="row">Name </th>
@@ -48,19 +83,61 @@ fetchContacts();
             </thead>
             <tbody>
               {contacts.map((contact) => (
-                  <tr key={contact._id}>
-                <th scope="row">{contact.name}</th>
-                <td>{contact.address}</td>
-                <td>{contact.email}</td>
-                <td>{contact.phone}</td>
-              </tr> 
+                <tr key={contact._id}
+                onClick={() => {
+                  setModalData([]);
+                  setModalData(contact);
+                  setShowModal(true);
+                }}>
+                  <th scope="row">{contact.name}</th>
+                  <td>{contact.address}</td>
+                  <td>{contact.email}</td>
+                  <td>{contact.phone}</td>
+                </tr>
               ))}
-              
             </tbody>
           </table>
-        ) }
-        
+        )}
       </div>
+
+      <Modal show={showModal} onHide={() => setShowModal(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>{modalData.name}</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          <h3>{modalData.name}</h3>
+          <p>
+            <strong>Address</strong>: {modalData.address}
+          </p>
+          <p>
+            <strong>Email</strong>: {modalData.email}
+          </p>
+          <p>
+            <strong>Phone Number</strong>: {modalData.phone}
+          </p>
+        </Modal.Body>
+
+        <Modal.Footer>
+          <Link className="btn-btn-info" to={`/edit/${modalData._id}`}>
+            Edit
+          </Link>
+
+          <button
+            className="btn-btn-danger"
+            onClick={() => deleteContact(modalData._id)}
+          >
+            Delete
+          </button>
+
+          <button
+            className="btn-btn-warning"
+            onClick={() => setShowModal(false)}
+          >
+            Close
+          </button>
+        </Modal.Footer>
+      </Modal>
     </>
   );
 };
